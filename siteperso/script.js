d9ef826666f416dc65d9a6e4f6f6bdaba022e5ba
pyrcl090 <70988666+pyrcl090@users.noreply.github.com>
2021-08-13 14:30:49 -0400

    Add files via upload

---
+++ siteperso/script.js
@@ -0,0 +1,66 @@
+console.log("hello");
+
+//toggle on and off for title and image+description
+var rageUn = document.querySelector(".rage1");
+var rageDeux = document.querySelector(".rage2");
+rageUn.addEventListener("click", function(){
+
+    rageDeux.classList.toggle("active");
+
+})
+
+var heidUn = document.querySelector(".heid1");
+var heidDeux = document.querySelector(".heid2");
+heidUn.addEventListener("click", function(){
+
+    heidDeux.classList.toggle("active");
+
+})
+
+var idkUn = document.querySelector(".idk1");
+var idkDeux = document.querySelector(".idk2");
+idkUn.addEventListener("click", function(){
+
+    idkDeux.classList.toggle("active");
+
+})
+
+var nouilleUn = document.querySelector(".nouille1");
+var nouilleDeux = document.querySelector(".nouille2");
+nouilleUn.addEventListener("click", function(){
+
+    nouilleDeux.classList.toggle("active");
+
+})
+
+var speakUn = document.querySelector(".speak1");
+var speakDeux = document.querySelector(".speak2");
+speakUn.addEventListener("click", function(){
+
+    speakDeux.classList.toggle("active");
+
+})
+
+var brokenUn = document.querySelector(".broken1");
+var brokenDeux = document.querySelector(".broken2");
+brokenUn.addEventListener("click", function(){
+
+    brokenDeux.classList.toggle("active");
+
+})
+
+var aboutUn = document.querySelector(".about1");
+var aboutDeux = document.querySelector(".about2");
+aboutUn.addEventListener("click", function(){
+
+    aboutDeux.classList.toggle("active");
+
+})
+
+var contactUn = document.querySelector(".contact1");
+var contactDeux = document.querySelector(".contact2");
+contactUn.addEventListener("click", function(){
+
+    contactDeux.classList.toggle("active");
+
+})
